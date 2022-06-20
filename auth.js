0a307222bc0f29c615e08fc04e866cca8e84fc68
unknown <faizeraza2468@gmail.com>
2022-06-21 02:09:06 +0500

    AllApi

--- auth.js
+++ auth.js
@@ -4,7 +4,37 @@ const { employee } = require("./dbase/modules");
 
 var app = express.Router()
 
+// login
 
+app.post('/login', (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+        res.status(403).send(
+            `please send email and passwod in json body.
+            e.g:
+             {
+            "email": "Razamalik@gmail.com",
+            "password": "abc",
+         }`)
+        return;
+    }
+    employee.findOne({ employeeEmail: req.body.email },
+        function (err, doc) {
+            console.log(doc)
+            if (!err) {
+                if (req.body.password === doc.employeePassword) {
+
+                    res.send(doc);
+                } 
+                
+            } else {
+                res.status(403).send({
+                    message: "Empolyee not found"
+                });
+            }
+        })
+})
+
+// Create Empolyee
 
 app.post('/employe', (req, res, next) => {
     if (!req.body.email || !req.body.password) {
@@ -36,7 +66,7 @@ app.post('/employe', (req, res, next) => {
     }
 })
 
-
+// Super Admin 
 app.get('/employe', (req, res, next) => {
     employee.find({ Role: "Awaiting" }, (err, data) => {
         if (!err) {
@@ -48,6 +78,7 @@ app.get('/employe', (req, res, next) => {
     })
 })
 
+//Post Admin 
 
 app.post('/AdminEmploye', (req, res, next) => {
     employee.findById({ _id: req.body.id },
@@ -71,7 +102,7 @@ app.post('/AdminEmploye', (req, res, next) => {
         })
 })
 
-
+// Get Admin
 
 app.get('/AdminEmploye', (req, res, next) => {
     employee.find({ Role: "Admin" }, (err, data) => {
@@ -84,8 +115,33 @@ app.get('/AdminEmploye', (req, res, next) => {
     })
 })
 
-app.get('/employe', (req, res, next) => {
-    employee.find({ Role: "Cashir" }, (err, data) => {
+// Post  Cashier
+app.post('/CashierEmploye', (req, res, next) => {
+    employee.findById({ _id: req.body.id },
+        (err, data) => {
+            if (data) {
+
+                data.updateOne({ Role: req.body.Role, Role: "Cashier" },
+                    (err, updatestatus) => {
+                        if (updatestatus) {
+                            res.send({ message: "Admin created Successfully!" })
+
+                        } else {
+                            res.send(err, "ERROR")
+                        }
+                    })
+            } else {
+                res.status(409).send({
+                    message: "employee Not Find"
+                })
+            }
+        })
+})
+
+// Get Cashier
+
+app.get('/CashierEmploye', (req, res, next) => {
+    employee.find({ Role: "Cashier" }, (err, data) => {
         if (!err) {
             res.send(data);
         }
@@ -95,7 +151,33 @@ app.get('/employe', (req, res, next) => {
     })
 })
 
-app.get('/employe', (req, res, next) => {
+// Post Rider
+
+app.post('/RiderEmploye', (req, res, next) => {
+    employee.findById({ _id: req.body.id },
+        (err, data) => {
+            if (data) {
+
+                data.updateOne({ Role: req.body.Role, Role: "Rider" },
+                    (err, updatestatus) => {
+                        if (updatestatus) {
+                            res.send({ message: "Admin created Successfully!" })
+
+                        } else {
+                            res.send(err, "ERROR")
+                        }
+                    })
+            } else {
+                res.status(409).send({
+                    message: "employee Not Find"
+                })
+            }
+        })
+})
+
+//  get Rider
+
+app.get('/RiderEmploye', (req, res, next) => {
     employee.find({ Role: "Rider" }, (err, data) => {
         if (!err) {
             res.send(data);
@@ -107,69 +189,6 @@ app.get('/employe', (req, res, next) => {
 })
 
 
-
-app.post('/login', (req, res, next) => {
-    if (!req.body.email || !req.body.password) {
-        res.status(403).send(
-            `please send email and passwod in json body.
-            e.g:
-             {
-            "email": "Razamalik@gmail.com",
-            "password": "abc",
-         }`)
-        return;
-    }
-    employee.findOne({ email: req.body.email },
-        function (err, doc) {
-            if (err) {
-
-                res.status(500).send({ message: "an error accure" })
-            } 
-            else if (doc) {
-
-                // bcrypt.varifyHash(req.body.password, doc.password).then(result => {
-                if (doc) {
-
-                    // console.log("matched");
-                    // var token = jwt.sign({
-                    //     id: doc._id,
-                    //     name: doc.name,
-                    //     email: doc.email,
-                    //     role: doc.role,
-                    // }, ServerSecretKey);
-
-                    // res.cookie('jToken', token, {
-                    //     maxAge: 86_400_000,
-                    //     httpOnly: true
-                    // });
-
-                    res.send({
-                        message: "login success",
-                     doc
-                        // doc: {
-                        //     name: doc.name,
-                        //     email: doc.email,
-                        //     role: doc.role,
-                        // },
-                        // token: token
-                    })
-                } 
-                else {
-                    console.log("not matched");
-                    res.status(401).send({
-                        message: "incorrect password"
-                    })
-                }
-                // }).catch(e => {
-                //     console.log("error: ", e)
-                // })
-            } else {
-                res.status(403).send({
-                    message: "user not found"
-                });
-            }
-        })
-})
 
 
 // =======================export
