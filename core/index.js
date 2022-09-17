188a3d98814fea792613d520df62b1c465417129
MuhammadHammadAhmed <42751169+MuhammadHammadAhmed@users.noreply.github.com>
2022-09-17 13:06:49 +0500

    changed db url

--- core/index.js
+++ core/index.js
@@ -1,6 +1,6 @@
 
 const PORT = process.env.PORT || 5000;
-const dbURI = process.env.dbURI || "mongodb+srv://toys:2468@toyscluster.02awi.mongodb.net/?retryWrites=true&w=majority";
+const dbURI = process.env.dbURI || "mongodb+srv://kollectit:1234@kollectit.ig3fyvk.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {
     PORT: PORT,
